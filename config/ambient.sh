5bea97e22d41cec401742a6a944bd14904b416b9
Diego Correa <diegocorrea.cc@gmail.com>
2017-09-15 02:09:21 -0300

    aptitude

--- config/ambient.sh
+++ config/ambient.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Bem vindo ao script de ambientação do Ouvido Musical"
 echo "São x passos ate a ambientação. Um arquivo de log sera gerado."
-sudo apt update > ambientation.log
-sudo apt install python3-pip python3-dev libpq-dev postgresql postgresql-contrib > ambientation.log
+sudo apt-get install aptitude
+sudo aptitude update > ambientation.log
+sudo aptitude install python3-pip python3-dev libpq-dev postgresql postgresql-contrib > ambientation.log
 sudo -u postgres psql < DB.sql > ambientation.log
 source ouvidoMusicalenv/bin/activate > ambientation.log
 pip install django psycopg2 > ambientation.log
