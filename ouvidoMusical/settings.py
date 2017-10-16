d7e48d8e988514ad1f21118a10d991d5638bb809
Diego Correa <diegocorrea.cc@gmail.com>
2017-10-16 13:20:11 -0300

    estabilizando arquivos

--- ouvidoMusical/settings.py
+++ ouvidoMusical/settings.py
@@ -85,8 +85,7 @@ WSGI_APPLICATION = 'ouvidoMusical.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'ouvidomusicalsmaller',
-        'NAME': 'ouvidomusicalbigger',
+        'NAME': 'ouvidomusicaltrain',
         'USER': 'ouvidomusicalapi',
         'PASSWORD': 'lovelovelove',
         'HOST': 'localhost',
