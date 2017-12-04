4d9c943675ad816e41959e6020ea694d28afd826
Diego Correa <diegocorrea.cc@gmail.com>
2017-12-04 17:56:55 -0300

    adicionando leitura de media e mediana e adicionando as mesmas tbm no boxplot

--- api/analysis/userPlaySong.py
+++ api/analysis/userPlaySong.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas
+import numpy as np
 
 from api.userPlaySong.models import UserPlaySong
 
@@ -7,5 +8,11 @@ def heardBoxPlot():
     plt.figure()
     heard = { 'play': [ ups.play_count for ups in UserPlaySong.objects.all()]}
     df = pandas.DataFrame(data=heard)
-    df.boxplot(column='play')
-    plt.savefig('heardBoxPlot.png')
+    df.boxplot(column='play', meanline=True, showmeans=True)
+    plt.savefig('./api/analysis/heardBoxPlot.png')
+
+def playCountData():
+    playList = [ ups.play_count for ups in UserPlaySong.objects.all()]
+    playMean = np.mean(playList)
+    playMedian = np.median(playList)
+    return {'mean': playMean, 'median': playMedian}
