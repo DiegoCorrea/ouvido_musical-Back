f74d29134f0dacebc0afa752c926856a86c2b4dd
Diego Correa <diegocorrea.cc@gmail.com>
2017-12-05 01:57:41 -0300

    ndcg gerando graficos de barra

--- api/analysis/graphs.py
+++ api/analysis/graphs.py
@@ -1,24 +1,26 @@
 import matplotlib.pyplot as plt
 import pandas
 import numpy as np
+from collections import Counter
 
 from api.userPlaySong.models import UserPlaySong
 from api.songs.models import SongSimilarity
+from api.userSongRecommendation.models import UserSongRecommendation
 
-def playCount():
-    plt.figure()
-    heard = {'play': [ups.play_count for ups in UserPlaySong.objects.all()]}
+def playCountBoxPlot():
+    heard = {'Ouvidas': [ups.play_count for ups in UserPlaySong.objects.all()]}
     df = pandas.DataFrame(data=heard)
+    plt.figure()
     plt.title('Quantidade de vezes que a música foi ouvida')
-    df.boxplot(column='play', meanline=True, showmeans=True)
+    df.boxplot(column='Ouvidas', meanline=True, showmeans=True)
     plt.savefig('./api/analysis/playCountBoxPlot.png')
 
 def titleSimilarityScatter():
-    plt.figure()
     songSimi = [songSimi.similarity for songSimi in SongSimilarity.objects.all()]
     songSimi.sort()
     simiMean = np.mean(songSimi)
     simiMedian = np.median(songSimi)
+    plt.figure()
     plt.title('Similaridade entre as músicas')
     plt.text(-0.1, 1, r'Media: ' + str(simiMean))
     plt.text(-0.1, .8, r'Mediana: ' + str(simiMedian))
@@ -28,3 +30,13 @@ def titleSimilarityScatter():
     plt.scatter(songSimi, songSimi)
     plt.axis([-0.2, 1.2, -0.2, 1.2])
     plt.savefig('./api/analysis/titleSimilarityScatter.png')
+
+def ndcgBar():
+    avaliations = [rec.score for rec in UserSongRecommendation.objects.all()]
+    z = Counter(avaliations)
+    plt.figure()
+    plt.xlabel('Nota')
+    plt.ylabel('Avaliações')
+    plt.title('NDCG - Quantidade de avaliações por nota')
+    plt.bar([0,1,2,3],[z[0],z[1],z[2],z[3]])
+    plt.savefig('./api/analysis/ndcgBar.png')
