fd41eaf1d3f0eba926f75c7bc91c44ac407e93f3
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-07 16:21:43 -0300

    mudando esquema de pastas dos graficos de map algoritmo

--- apps/evaluators/MAP/analyzer/algorithm.py
+++ apps/evaluators/MAP/analyzer/algorithm.py
@@ -7,9 +7,6 @@ from django.db import connection
 import logging
 
 logger = logging.getLogger(__name__)
-directory = str('./apps/evaluators/MAP/analyzer/graphs/' + str(connection.settings_dict['NAME'] + '/algorithm/'))
-if not os.path.exists(directory):
-    os.makedirs(directory)
 
 def value_gLine(at=5):
     logger.info("[Start MAP Value (Graph Line)]")
@@ -24,6 +21,9 @@ def value_gLine(at=5):
     logger.debug("MAP Evaluation -> Mean: " + str(evaluationMeanValues[-1]))
     logger.debug("MAP Evaluation -> Median: " + str(evaluationMedianValues[-1]))
     logger.debug("MAP Evaluation -> Run Number: " + str(len(evaluationValues)))
+    directory = str('./apps/evaluators/MAP/analyzer/graphs/' + str(connection.settings_dict['NAME']) + '/algorithm/' + str(allEvaluations.last().id) + '/')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     plt.figure()
     plt.grid(True)
     plt.title('MAP - Mean Averange Precision@' + str(at))
@@ -33,7 +33,7 @@ def value_gLine(at=5):
     plt.plot([evaluation.id for evaluation in allEvaluations],[evaluation for evaluation in evaluationMeanValues],color='green',label='Media')
     plt.plot([evaluation.id for evaluation in allEvaluations],[evaluation for evaluation in evaluationMedianValues],color='blue',label='Mediana')
     plt.legend(loc='best')
-    plt.savefig(str(directory) + 'value_gLine' + '-[id]-'+ str(allEvaluations.last().id) + '.png')
+    plt.savefig(str(directory) + 'value_gLine.png')
     logger.info("[Finish MAP Value (Graph Line)]")
 def value_gScatter(at=5):
     logger.info("[Start MAP Value (Graph Scatter)]")
@@ -48,6 +48,9 @@ def value_gScatter(at=5):
     logger.debug("MAP Evaluation -> Mean: " + str(evaluationMeanValues[-1]))
     logger.debug("MAP Evaluation -> Median: " + str(evaluationMedianValues[-1]))
     logger.debug("MAP Evaluation -> Run Number: " + str(len(evaluationValues)))
+    directory = str('./apps/evaluators/MAP/analyzer/graphs/' + str(connection.settings_dict['NAME']) + '/algorithm/' + str(allEvaluations.last().id) + '/')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     plt.figure()
     plt.grid(True)
     plt.title('MAP - Mean Averange Precision@' + str(at))
@@ -55,17 +58,20 @@ def value_gScatter(at=5):
     plt.xlabel('Valor MAP')
     plt.scatter(evaluationValues, evaluationValues, label='Media: ' + str(float("{0:.4f}".format(evaluationValues[-1]))))
     plt.legend(loc='upper left')
-    plt.savefig(str(directory) + 'value_gScatter' + '-[id]-'+ str(allEvaluations.last().id) + '.png')
+    plt.savefig(str(directory) + 'value_gScatter.png')
     logger.info("[Finish MAP Value (Graph Scatter)]")
 def value_gBoxPlot(at=5):
     logger.info("[Start MAP Value (Graph BoxPlot)]")
     allEvaluations = MAP.objects.filter(limit=at)
     evaluationValues = [(evalution.value) for evalution in allEvaluations]
     logger.debug("MAP Evaluation -> Run Number: " + str(len(evaluationValues)))
+    directory = str('./apps/evaluators/MAP/analyzer/graphs/' + str(connection.settings_dict['NAME']) + '/algorithm/' + str(allEvaluations.last().id) + '/')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     plt.figure()
     plt.title('MAP - Mean Averange Precision@' + str(at))
     plt.boxplot(evaluationValues, labels='V')
-    plt.savefig(str(directory) + 'value_gBoxPlot' + '-[id]-'+ str(allEvaluations.last().id) + '.png')
+    plt.savefig(str(directory) + 'value_gBoxPlot.png')
     logger.info("[Finish MAP Value (Graph BoxPlot)]")
 def value_gBar(at=5):
     logger.info("[Start MAP Value (Graph Bar)]")
@@ -74,11 +80,14 @@ def value_gBar(at=5):
     evalutionCountList = Counter(evaluationValues)
     mode = evalutionCountList.most_common(1)[0][0]
     logger.debug('MAP Evaluation -> Mode: ' + str(mode))
+    directory = str('./apps/evaluators/MAP/analyzer/graphs/' + str(connection.settings_dict['NAME']) + '/algorithm/' + str(allEvaluations.last().id) + '/')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     plt.figure()
     plt.title('MAP - Mean Averange Precision@' + str(at))
     plt.ylabel('Intervalor de valores')
     plt.xlabel('Quantidade')
     plt.bar(evalutionCountList.values(),evalutionCountList.keys(), label='Moda: ' + str(float("{0:.4f}".format(mode))))
     plt.legend(loc='best')
-    plt.savefig(str(directory) + 'value_gBar' + '-[id]-'+ str(allEvaluations.last().id) + '.png')
+    plt.savefig(str(directory) + 'value_gBar.png')
     logger.info("[Finish MAP Value (Graph Bar)]")
