c7c63033032e4ff892cc6ed9d19add78b28677ff
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-07 16:32:45 -0300

    adicionando analizado do MRR

---
+++ apps/evaluators/MRR/analyzer/views.py
@@ -0,0 +1,13 @@
+from .algorithm import value_gLine, value_gScatter, value_gBoxPlot, value_gBar
+from .benchmark import bench_gBar, bench_gBoxPlot, bench_gLine, bench_gScatter
+
+def runMAPValueGraph():
+    value_gLine()
+    value_gScatter()
+    value_gBoxPlot()
+    value_gBar()
+def runMAPBenchmarkGraph():
+    bench_gBar()
+    bench_gLine()
+    bench_gBoxPlot()
+    bench_gScatter()
