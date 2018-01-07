ecb8c492454706e080fac25582b94252a98cb55f
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-07 01:44:54 -0300

    graficos

--- apps/evaluators/MAP/analyzer/benchmark.py
+++ apps/evaluators/MAP/analyzer/benchmark.py
@@ -4,16 +4,21 @@ import os
 from collections import Counter
 from apps.evaluators.MAP.benchmark.models import BenchMAP
 from django.db import connection
+import logging
 
+logger = logging.getLogger(__name__)
 directory = str('./apps/evaluators/MAP/analyzer/graphs/' + str(connection.settings_dict['NAME'] + '/benchmark/'))
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 def bench_gLine():
+    logger.info("[Start Bench Graph Line]")
     allBenchmarks = BenchMAP.objects.all()
     benchmarkTimes = [((benchmark.finished_at - benchmark.started_at).total_seconds() / 60.0) for benchmark in allBenchmarks]
     benchmarkMean = np.mean(benchmarkTimes)
     benchmarkMedian = np.median(benchmarkTimes)
+    logger.debug("MAP Benchmark Time Mean: " + str(benchmarkMean))
+    logger.debug("MAP Benchmark Time Median: " + str(benchmarkMedian))
     plt.figure()
     plt.grid(True)
     plt.title('MAP Benchmark')
@@ -23,12 +28,15 @@ def bench_gLine():
     plt.text(-0.1, .8, r'Mediana: ' + str(float("{0:.2f}".format(benchmarkMedian))))
     plt.plot([benchmark.id for benchmark in allBenchmarks],[benchmark for benchmark in benchmarkTimes])
     plt.savefig(str(directory) + 'value_gLine' + '-[id]-'+ str(allBenchmarks.last().id) + '.png')
-
+    logger.info("[Finish Bench Graph Line]")
 def bench_gScatter(at=5):
+    logger.info("[Start Bench Graph Scatter]")
     allBenchmarks = BenchMAP.objects.all()
     benchmarkTimes = [((benchmark.finished_at - benchmark.started_at).total_seconds() / 60.0) for benchmark in allBenchmarks]
     benchmarkMean = np.mean(benchmarkTimes)
     benchmarkMedian = np.median(benchmarkTimes)
+    logger.debug("MAP Benchmark Time Mean: " + str(benchmarkMean))
+    logger.debug("MAP Benchmark Time Median: " + str(benchmarkMedian))
     plt.figure()
     plt.grid(True)
     plt.title('MAP Benchmark')
@@ -38,16 +46,18 @@ def bench_gScatter(at=5):
     plt.text(-0.1, .8, r'Mediana: ' + str(float("{0:.2f}".format(benchmarkMedian))))
     plt.scatter(benchmarkTimes, benchmarkTimes)
     plt.savefig(str(directory) + 'value_gScatter' + '-[id]-'+ str(allBenchmarks.last().id) + '.png')
-
+    logger.info("[Finish Bench Graph Scatter]")
 def bench_gBoxPlot(at=5):
+    logger.info("[Start Bench Graph BoxPlot]")
     allBenchmarks = BenchMAP.objects.all()
     benchmarkTimes = [((benchmark.finished_at - benchmark.started_at).total_seconds() / 60.0) for benchmark in allBenchmarks]
     plt.figure()
     plt.title('MAP Benchmark')
     plt.boxplot(benchmarkTimes)
     plt.savefig(str(directory) + 'value_gBoxPlot' + '-[id]-'+ str(allBenchmarks.last().id) + '.png')
-
+    logger.info("[Finish Bench Graph BoxPlot]")
 def bench_gBar(at=5):
+    logger.info("[Start Bench Graph Bar]")
     allBenchmarks = BenchMAP.objects.all()
     benchmarkTimes = [float("{0:.1f}".format((benchmark.finished_at - benchmark.started_at).total_seconds())) for benchmark in allBenchmarks]
     benchmarkCountList = Counter(benchmarkTimes)
@@ -57,3 +67,4 @@ def bench_gBar(at=5):
     plt.xlabel('Quantidade')
     plt.bar(benchmarkCountList.values(),benchmarkCountList.keys())
     plt.savefig(str(directory) + 'value_gBar' + '-[id]-'+ str(allBenchmarks.last().id) + '.png')
+    logger.info("[Finish Bench Graph Bar]")
