560225d235921e7d4f94c417cac8416ced3bd493
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-12 15:49:41 -0300

    dependencia um pra um

--- apps/recommenders/UserAverage/benchmark/models.py
+++ apps/recommenders/UserAverage/benchmark/models.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from apps.recommenders.UserAverage.algorithm.models import UserAverage_Life
 
 # Create your models here.
 class BenchUserAverage(models.Model):
+    life = models.OneToOneField(UserAverage_Life)
     started_at = models.DateTimeField()
     finished_at = models.DateTimeField()
