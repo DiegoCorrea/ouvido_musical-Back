7841b0b373e2840f153ccf35321cc78e4951f44d
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-15 22:08:27 -0300

    alterando recomender

--- apps/recommenders/UserAverage/algorithm/algorithm.py
+++ apps/recommenders/UserAverage/algorithm/algorithm.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from random import choice, randint
+from random import choice, randint, sample
 from django.db import transaction
 
 from apps.CONSTANTS import MAX_SCORE, MIN_SCORE
@@ -13,39 +13,32 @@ logger = logging.getLogger(__name__)
 ################################################################################
 # User Average uses the average rating value of a user to make predictions.
 #
-def getUserAverageRecommendationsAllDB(user_id):
-    recommendation = {}
-    for songPlayed in UserPlaySong.objects.filter(user_id=user_id).order_by('play_count').reverse():
-        for songSimi in songPlayed.song.getSimilaries():
-            if songSimi.similarity == 0.0: continue
-            if songSimi.songCompare not in recommendation:
-                recommendation.setdefault(songSimi.songCompare, [])
-            recommendation[songSimi.songCompare].append(songSimi.similarity)
-    rec = {}
-    for (song, values) in recommendation.items():
-        rec.setdefault(song, sum(values)/len(values))
-    return OrderedDict(sorted(rec.items(), key=lambda t: t[1], reverse=True))
-
-def getUserAverageRecommendationsLimited(user_id, songSetLimit):
-    recommendation = {}
-    songIDList = [ song.id for song in Song.objects.all()[:songSetLimit]]
-    for songPlayed in UserPlaySong.objects.filter(user_id=user_id).order_by('play_count').reverse():
+def getUserAverageRecommendations(user_id, songSetLimit, songIDList):
+    recommendation = { }
+    songPlayedID_list = [ played.song_id for played in UserPlaySong.objects.filter(user_id=user_id)]
+    songIDList = songIDList.exclude(songPlayedID_list)
+    try:
+        songIDList = sample(set(songIDList), songSetLimit)
+    except ValueError as e:
+        songIDList = sample(set(songIDList), len(songIDList))
+    for songPlayed in userPlayed_list:
         for songSimi in songPlayed.song.getSimilaries(songIDList):
             if songSimi.similarity == 0.0: continue
             if songSimi.songCompare not in recommendation:
                 recommendation.setdefault(songSimi.songCompare, [])
             recommendation[songSimi.songCompare].append(songSimi.similarity)
-    rec = {}
+    rec = { }
     for (song, values) in recommendation.items():
         rec.setdefault(song, sum(values)/len(values))
     return OrderedDict(sorted(rec.items(), key=lambda t: t[1], reverse=True))
-
-def UserAverage(userList=User.objects.all(), songSetLimit=Song.objects.count()):
+def UserAverage(userList=User.objects.all(), songSetLimit=Song.objects.count(), allSongs=Song.objects.all()):
+    bool(allSongs)
+    songIDList = [ song.id for song in allSongs ]
     UserAverage_Life.objects.create(setSize=songSetLimit)
     logger.info("[Start User Average]")
     with transaction.atomic():
         for user in userList:
-            userRecommendations = getUserAverageRecommendationsLimited(user.id, songSetLimit)
+            userRecommendations = getUserAverageRecommendations(user.id, songSetLimit, songIDList=songIDList)
             for (song, similarity) in userRecommendations.items():
                 UserAverage_Recommendations.objects.create(
                             song=Song.objects.get(id=song.id),
