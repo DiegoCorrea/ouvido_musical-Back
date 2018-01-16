36649eb6c8e06f1e891b745f45e63c60774466b7
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-16 00:42:57 -0300

    trocando parametros

--- apps/evaluators/views.py
+++ apps/evaluators/views.py
@@ -1,11 +1,8 @@
 from .MAP.algorithm.views import runMAP
 from .MRR.algorithm.views import runMRR
 from .NDCG.algorithm.views import runNDCG
-from .MAP.analyzer.views import runAllMAPAnalizers
-from .MRR.analyzer.views import runAllMRRAnalizers
-from .NDCG.analyzer.views import runAllNDCGAnalizers
 
-def runEvaluations(at=5,songSetLimit):
+def runEvaluations(songSetLimit,at=5):
     runMAP(at=at)
     #runAllMAPAnalizers(at=at,songSetLimit=songSetLimit)
     runMRR(at=at)
