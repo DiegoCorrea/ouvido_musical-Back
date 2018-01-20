d0a18cc5324135a916f0dd29d603110ba33e1535
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-20 16:31:21 -0300

    dividindo interação de salvamento

--- apps/similarities/Cosine/algorithm/views.py
+++ apps/similarities/Cosine/algorithm/views.py
@@ -7,38 +7,50 @@ from django.db import transaction
 
 import logging
 logger = logging.getLogger(__name__)
+
+
+def saveTitleSimilarity(similarityMatrix, sBase, songInterator):
+    for sComp in songInterator:
+        SongSimilarity.objects.create(
+            songBase=sBase['id'],
+            songCompare=songInterator[sComp]['id'],
+            similarity=similarityMatrix[sBase['position']][songInterator[sComp]['position']]
+        )
+
+
 def TitleSimilarity():
     logger.info("[Start Title Similarity]")
     allSongs = Song.objects.all()
-    bool(allSongs)
-    similarSongs = { }
-    newSongs = 0
-    # Calc Cosine from Songs and return a symmetric matrix len(Songs) x len(Songs)
-    similarityMatrix = CosineSimilarity([ song.title for song in allSongs ])
-    # Load previous Songs similarities
-    logger.info("Loading previous Songs similarities")
-    for similar in SongSimilarity.objects.all():
-        similarSongs.setdefault(similar.songBase_id, { })
-        similarSongs[similar.songBase_id].setdefault(similar.songCompare_id, similar.similarity)
+    songPosition = {}
+    line = 0
+    similarityMatrix = CosineSimilarity([song.title for song in allSongs])
+    for song in allSongs:
+        songPosition.setdefault(song.id, {
+            'id': song,
+            'position': line
+            }
+        )
+        line += 1
     # Persiste Title similarity
     logger.info("Start to persiste Title similarity")
-    line = 0
-    allHighSongs = allSongs
     with transaction.atomic():
-        for songBase in allHighSongs:
-            allSongs = allSongs.exclude(id=songBase.id)
-            column = line + 1
-            for songCompare in allSongs:
-                SongSimilarity.objects.create(songBase=songBase, songCompare=songCompare, similarity=similarityMatrix[line][column])
-                column += 1
-            line += 1
-    logger.debug("Total DB songs: %d", line)
+        for songBase in allSongs:
+            sB = songPosition[songBase.id]
+            del songPosition[songBase.id]
+            logger.info("++ Song: " + str(sB['position']))
+            saveTitleSimilarity(similarityMatrix, sB, songPosition)
     logger.info("[Finish Title Similarity]")
+
+
 def runTitleSimilarity():
     logger.info("[Start Title Similarity with Cosine] - Benchmark")
     startedAt = timezone.now()
     TitleSimilarity()
     finishedAt = timezone.now()
-    BenchCosine_SongTitle.objects.create(started_at=startedAt,finished_at=finishedAt)
-    logger.info("Benchmark: Start at - " + str(startedAt) + " || Finished at -" + str(finishedAt))
+    BenchCosine_SongTitle.objects.create(
+        started_at=startedAt, finished_at=finishedAt
+    )
+    logger.info(
+        "Benchmark: Start at - " + str(startedAt)
+        + " || Finished at -" + str(finishedAt))
     logger.info("[Finish Title Similarity with Cosine] - Benchmark")
