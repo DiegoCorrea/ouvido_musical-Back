114c8f8bad292864313adbf6977f880024dc383d
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-21 15:54:52 -0300

    identando e aceitando parametros variados

--- apps/evaluators/NDCG/analyzer/algorithm.py
+++ apps/evaluators/NDCG/analyzer/algorithm.py
@@ -8,90 +8,213 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def value_gLine(at=5):
+
+def value_gLine(at=5,songSetLimit):
     logger.info("[Start NDCG Value (Graph Line)]")
-    allEvaluations = NDCG.objects.filter(at=at)
-    evaluationValues = [ ]
-    evaluationMeanValues = [ ]
-    evaluationMedianValues = [ ]
+    allEvaluations = NDCG.objects.filter(
+        at=at
+    ).filter(life.setSize=songSetLimit)
+    evaluationValues = []
+    evaluationMeanValues = []
+    evaluationMedianValues = []
     for evaluation in allEvaluations:
         evaluationValues.append(evaluation.value)
         evaluationMeanValues.append(np.mean(evaluationValues))
         evaluationMedianValues.append(np.median(evaluationValues))
-    logger.debug("NDCG Evaluation -> Mean: " + str(evaluationMeanValues[-1]))
-    logger.debug("NDCG Evaluation -> Median: " + str(evaluationMedianValues[-1]))
-    logger.debug("NDCG Evaluation -> Run Number: " + str(len(evaluationValues)))
-    directory = str('./files/apps/evaluators/NDCG/graphs/' + str(connection.settings_dict['NAME']) + '/algorithm/' + str(allEvaluations.last().id) + '/')
+    logger.debug(
+        "NDCG Evaluation -> Mean: "
+        + str(evaluationMeanValues[-1])
+    )
+    logger.debug(
+        "NDCG Evaluation -> Median: "
+        + str(evaluationMedianValues[-1])
+    )
+    logger.debug(
+        "NDCG Evaluation -> Run Number: "
+        + str(len(evaluationValues))
+    )
+    directory = str(
+        './files/apps/evaluators/NDCG/graphs/'
+        + str(songSetLimit)
+        + '/algorithm/'
+        + str(at)
+        + '/'
+    )
     if not os.path.exists(directory):
         os.makedirs(directory)
     plt.figure()
     plt.grid(True)
-    plt.title('NDCG - Normalize Discounted Cumulative Gain@' + str(at))
+    plt.title(
+        'NDCG - Normalize Discounted Cumulative Gain@'
+        + str(at)
+        + '\nSet - '
+        + str(songSetLimit)
+    )
     plt.xlabel('ID do execução')
     plt.ylabel('Valor do NDCG')
-    plt.plot([evaluation.id for evaluation in allEvaluations],[evaluation for evaluation in evaluationValues],color='red',label='Valor')
-    plt.plot([evaluation.id for evaluation in allEvaluations],[evaluation for evaluation in evaluationMeanValues],color='green',label='Media')
-    plt.plot([evaluation.id for evaluation in allEvaluations],[evaluation for evaluation in evaluationMedianValues],color='blue',label='Mediana')
+    plt.plot(
+        [evaluation.id for evaluation in allEvaluations],
+        [evaluation for evaluation in evaluationValues],
+        color='red',
+        label='Valor'
+    )
+    plt.plot(
+        [evaluation.id for evaluation in allEvaluations],
+        [evaluation for evaluation in evaluationMeanValues],
+        color='green',
+        label='Media'
+    )
+    plt.plot(
+        [evaluation.id for evaluation in allEvaluations],
+        [evaluation for evaluation in evaluationMedianValues],
+        color='blue',
+        label='Mediana'
+    )
     plt.legend(loc='best')
-    plt.savefig(str(directory) + 'value_gLine-[' + str(at) + '].png')
+    plt.savefig(
+        str(directory)
+        + 'value_gLine.png'
+    )
     plt.close()
     logger.info("[Finish NDCG Value (Graph Line)]")
-def value_gScatter(at=5):
+
+
+def value_gScatter(at=5,songSetLimit):
     logger.info("[Start NDCG Value (Graph Scatter)]")
-    allEvaluations = NDCG.objects.filter(at=at)
-    evaluationValues = [ ]
-    evaluationMeanValues = [ ]
-    evaluationMedianValues = [ ]
+    allEvaluations = NDCG.objects.filter(
+        at=at
+    ).filter(life.setSize=songSetLimit)
+    evaluationValues = []
+    evaluationMeanValues = []
+    evaluationMedianValues = []
     for evaluation in allEvaluations:
         evaluationValues.append(evaluation.value)
         evaluationMeanValues.append(np.mean(evaluationValues))
         evaluationMedianValues.append(np.median(evaluationValues))
-    logger.debug("NDCG Evaluation -> Mean: " + str(evaluationMeanValues[-1]))
-    logger.debug("NDCG Evaluation -> Median: " + str(evaluationMedianValues[-1]))
-    logger.debug("NDCG Evaluation -> Run Number: " + str(len(evaluationValues)))
-    directory = str('./files/apps/evaluators/NDCG/graphs/' + str(connection.settings_dict['NAME']) + '/algorithm/' + str(allEvaluations.last().id) + '/')
+    logger.debug(
+        "NDCG Evaluation -> Mean: "
+        + str(evaluationMeanValues[-1])
+    )
+    logger.debug(
+        "NDCG Evaluation -> Median: "
+        + str(evaluationMedianValues[-1])
+    )
+    logger.debug(
+        "NDCG Evaluation -> Run Number: "
+        + str(len(evaluationValues))
+    )
+    directory = str(
+        './files/apps/evaluators/NDCG/graphs/'
+        + str(songSetLimit)
+        + '/algorithm/'
+        + str(at)
+        + '/'
+    )
     if not os.path.exists(directory):
         os.makedirs(directory)
     plt.figure()
     plt.grid(True)
-    plt.title('NDCG - Normalize Discounted Cumulative Gain@' + str(at))
+    plt.title(
+        'NDCG - Normalize Discounted Cumulative Gain@'
+        + str(at)
+        + '\nSet - '
+        + str(songSetLimit)
+    )
     plt.ylabel('Valor NDCG')
     plt.xlabel('Valor NDCG')
-    plt.scatter(evaluationValues, evaluationValues, label='Media: ' + str(float("{0:.4f}".format(evaluationValues[-1]))))
+    plt.scatter(
+        evaluationValues,
+        evaluationValues,
+        label='Media: '
+        + str(float("{0:.4f}".format(evaluationValues[-1])))
+    )
     plt.legend(loc='upper left')
-    plt.savefig(str(directory) + 'value_gScatter-[' + str(at) + '].png')
+    plt.savefig(
+        str(directory)
+        + 'value_gScatter.png'
+    )
     plt.close()
     logger.info("[Finish NDCG Value (Graph Scatter)]")
-def value_gBoxPlot(at=5):
+
+
+def value_gBoxPlot(at=5,songSetLimit):
     logger.info("[Start NDCG Value (Graph BoxPlot)]")
-    allEvaluations = NDCG.objects.filter(at=at)
-    evaluationValues = [(evalution.value) for evalution in allEvaluations]
-    logger.debug("NDCG Evaluation -> Run Number: " + str(len(evaluationValues)))
-    directory = str('./files/apps/evaluators/NDCG/graphs/' + str(connection.settings_dict['NAME']) + '/algorithm/' + str(allEvaluations.last().id) + '/')
+    allEvaluations = NDCG.objects.filter(
+        at=at
+    ).filter(life.setSize=songSetLimit)
+    evaluationValues = [
+        (evalution.value)
+        for evalution in allEvaluations
+    ]
+    logger.debug(
+        "NDCG Evaluation -> Run Number: "
+        + str(len(evaluationValues))
+    )
+    directory = str(
+        './files/apps/evaluators/NDCG/graphs/'
+        + str(songSetLimit)
+        + '/algorithm/'
+        + str(at)
+        + '/'
+    )
     if not os.path.exists(directory):
         os.makedirs(directory)
     plt.figure()
-    plt.title('NDCG - Normalize Discounted Cumulative Gain@' + str(at))
+    plt.title(
+        'NDCG - Normalize Discounted Cumulative Gain@'
+        + str(at)
+        + '\nSet - '
+        + str(songSetLimit)
+    )
     plt.boxplot(evaluationValues, labels='V')
-    plt.savefig(str(directory) + 'value_gBoxPlot-[' + str(at) + '].png')
+    plt.savefig(
+        str(directory)
+        + 'value_gBoxPlot.png'
+    )
     plt.close()
     logger.info("[Finish NDCG Value (Graph BoxPlot)]")
-def value_gBar(at=5):
+
+
+def value_gBar(at=5,songSetLimit):
     logger.info("[Start NDCG Value (Graph Bar)]")
-    allEvaluations = NDCG.objects.filter(at=at)
-    evaluationValues = [float("{0:.4f}".format(evalution.value)) for evalution in allEvaluations]
+    allEvaluations = NDCG.objects.filter(
+        at=at
+    ).filter(life.setSize=songSetLimit)
+    evaluationValues = [
+        float("{0:.4f}".format(evalution.value))
+        for evalution in allEvaluations
+    ]
     evalutionCountList = Counter(evaluationValues)
     mode = evalutionCountList.most_common(1)[0][0]
     logger.debug('NDCG Evaluation -> Mode: ' + str(mode))
-    directory = str('./files/apps/evaluators/NDCG/graphs/' + str(connection.settings_dict['NAME']) + '/algorithm/' + str(allEvaluations.last().id) + '/')
+    directory = str(
+        './files/apps/evaluators/NDCG/graphs/'
+        + str(songSetLimit)
+        + '/algorithm/'
+        + str(at)
+        + '/'
+    )
     if not os.path.exists(directory):
         os.makedirs(directory)
     plt.figure()
-    plt.title('NDCG - Normalize Discounted Cumulative Gain@' + str(at))
+    plt.title(
+        'NDCG - Normalize Discounted Cumulative Gain@'
+        + str(at)
+        + '\nSet - '
+        + str(songSetLimit)
+    )
     plt.ylabel('Intervalor de valores')
     plt.xlabel('Quantidade')
-    plt.bar(evalutionCountList.values(),evalutionCountList.keys(), label='Moda: ' + str(float("{0:.4f}".format(mode))))
+    plt.bar(
+        evalutionCountList.values(),
+        evalutionCountList.keys(),
+        label='Moda: '
+        + str(float("{0:.4f}".format(mode)))
+    )
     plt.legend(loc='best')
-    plt.savefig(str(directory) + 'value_gBar-[' + str(at) + '].png')
+    plt.savefig(
+        str(directory)
+        + 'value_gBar.png'
+    )
     plt.close()
     logger.info("[Finish NDCG Value (Graph Bar)]")
