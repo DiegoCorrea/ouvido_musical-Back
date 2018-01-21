fd5d5f41a6bcb8846ca7d76e4c4c2cd385be5e6e
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-21 12:57:17 -0300

    adicionando arquivos de benchmark do cosine, funções não tratadas ainda

--- apps/similarities/Cosine/benchmark/models.py
+++ apps/similarities/Cosine/benchmark/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
 class BenchCosine_SongTitle(models.Model):
     started_at = models.DateTimeField()
     finished_at = models.DateTimeField()
