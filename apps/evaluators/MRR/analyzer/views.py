8cad7871698a3d57630901b1f11a7e153169847b
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-21 14:23:35 -0300

    identando e passando parametros

--- apps/evaluators/MRR/analyzer/views.py
+++ apps/evaluators/MRR/analyzer/views.py
@@ -1,16 +1,31 @@
-from .algorithm import value_gLine, value_gScatter, value_gBoxPlot, value_gBar
-from .benchmark import bench_gBar, bench_gBoxPlot, bench_gLine, bench_gScatter
+from .algorithm import (
+    value_gLine,
+    value_gScatter,
+    value_gBoxPlot,
+    value_gBar
+)
+from .benchmark import (
+    bench_gBar,
+    bench_gBoxPlot,
+    bench_gLine,
+    bench_gScatter
+)
 
-def runMRRValueGraph(at=5):
-    value_gLine(at=5)
-    value_gScatter(at=at)
-    value_gBoxPlot(at=at)
-    value_gBar(at=at)
-def runMRRBenchmarkGraph():
-    bench_gBar()
-    bench_gLine()
-    bench_gBoxPlot()
-    bench_gScatter()
-def runAllMRRAnalizers(at=5):
-    runMRRValueGraph(at=at)
-    runMRRBenchmarkGraph()
+
+def runMRRValueGraph(at=5, setSongLimit):
+    value_gLine(at=at, setSongLimit=setSongLimit)
+    value_gScatter(at=at, setSongLimit=setSongLimit)
+    value_gBoxPlot(at=at, setSongLimit=setSongLimit)
+    value_gBar(at=at, setSongLimit=setSongLimit)
+
+
+def runMRRBenchmarkGraph(at=5, setSongLimit):
+    bench_gBar(at=at, setSongLimit=setSongLimit)
+    bench_gLine(at=at, setSongLimit=setSongLimit)
+    bench_gBoxPlot(at=at, setSongLimit=setSongLimit)
+    bench_gScatter(at=at, setSongLimit=setSongLimit)
+
+
+def runAllMRRAnalizers(at=5, setSongLimit):
+    runMRRValueGraph(at=at, setSongLimit=setSongLimit)
+    runMRRBenchmarkGraph(at=at, setSongLimit=setSongLimit)
