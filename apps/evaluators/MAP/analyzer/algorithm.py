8aa10a368e4a3e6f25b83f5720660aea2807c857
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-22 18:20:10 -0300

    retirando filtro errado

--- apps/evaluators/MAP/analyzer/algorithm.py
+++ apps/evaluators/MAP/analyzer/algorithm.py
@@ -3,18 +3,17 @@ import numpy as np
 import logging
 import os
 from collections import Counter
-from django.db import connection
 from apps.evaluators.MAP.algorithm.models import MAP
-from apps.recommenders.UserAverage.algorithm.models import UserAverage_Life
 
 logger = logging.getLogger(__name__)
 
 
 def value_gLine(songSetLimit, at=5):
     logger.info("[Start MAP Value (Graph Line)]")
-    allEvaluations = MAP.objects.filter(
-        at=at
-    ).filter(life.setSize=songSetLimit)
+    allEvaluations = []
+    for evalution in MAP.objects.filter(at=at):
+        if evalution.life.setSize == songSetLimit:
+            allEvaluations.append(evalution)
     evaluationValues = []
     evaluationMeanValues = []
     evaluationMedianValues = []
@@ -56,17 +55,20 @@ def value_gLine(songSetLimit, at=5):
     plt.plot(
         [evaluation.id for evaluation in allEvaluations],
         [evaluation for evaluation in evaluationValues],
-        color='red',label='Valor'
+        color='red',
+        label='Valor'
         )
     plt.plot(
         [evaluation.id for evaluation in allEvaluations],
         [evaluation for evaluation in evaluationMeanValues],
-        color='green',label='Media'
-        )
+        color='green',
+        label='Media'
+    )
     plt.plot(
         [evaluation.id for evaluation in allEvaluations],
         [evaluation for evaluation in evaluationMedianValues],
-        color='blue',label='Mediana'
+        color='blue',
+        label='Mediana'
     )
     plt.legend(loc='best')
     plt.savefig(str(directory) + 'value_gLine.png')
@@ -76,9 +78,10 @@ def value_gLine(songSetLimit, at=5):
 
 def value_gScatter(songSetLimit, at=5):
     logger.info("[Start MAP Value (Graph Scatter)]")
-    allEvaluations = MAP.objects.filter(
-        at=at
-    ).filter(life.setSize=songSetLimit)
+    allEvaluations = []
+    for evalution in MAP.objects.filter(at=at):
+        if evalution.life.setSize == songSetLimit:
+            allEvaluations.append(evalution)
     evaluationValues = []
     evaluationMeanValues = []
     evaluationMedianValues = []
@@ -109,14 +112,19 @@ def value_gScatter(songSetLimit, at=5):
         os.makedirs(directory)
     plt.figure()
     plt.grid(True)
-    plt.title('MAP - Mean Averange Precision@' + str(at) + '\nSet - '+ str(songSetLimit))
+    plt.title(
+        'MAP - Mean Averange Precision@'
+        + str(at)
+        + '\nSet - '
+        + str(songSetLimit)
+    )
     plt.ylabel('Valor MAP')
     plt.xlabel('Valor MAP')
     plt.scatter(
         evaluationValues,
         evaluationValues,
         label='Media: '
-            + str(float("{0:.4f}".format(evaluationValues[-1])))
+        + str(float("{0:.4f}".format(evaluationValues[-1])))
     )
     plt.legend(loc='upper left')
     plt.savefig(str(directory) + 'value_gScatter.png')
@@ -126,9 +134,10 @@ def value_gScatter(songSetLimit, at=5):
 
 def value_gBoxPlot(songSetLimit, at=5):
     logger.info("[Start MAP Value (Graph BoxPlot)]")
-    allEvaluations = MAP.objects.filter(
-        at=at
-    ).filter(life.setSize=songSetLimit)
+    allEvaluations = []
+    for evalution in MAP.objects.filter(at=at):
+        if evalution.life.setSize == songSetLimit:
+            allEvaluations.append(evalution)
     evaluationValues = [
         (evalution.value)
         for evalution in allEvaluations
@@ -147,7 +156,12 @@ def value_gBoxPlot(songSetLimit, at=5):
     if not os.path.exists(directory):
         os.makedirs(directory)
     plt.figure()
-    plt.title('MAP - Mean Averange Precision@' + str(at) + '\nSet - '+ str(songSetLimit))
+    plt.title(
+        'MAP - Mean Averange Precision@'
+        + str(at)
+        + '\nSet - '
+        + str(songSetLimit)
+    )
     plt.boxplot(evaluationValues, labels='V')
     plt.savefig(
         str(directory)
@@ -159,9 +173,10 @@ def value_gBoxPlot(songSetLimit, at=5):
 
 def value_gBar(songSetLimit, at=5):
     logger.info("[Start MAP Value (Graph Bar)]")
-    allEvaluations = MAP.objects.filter(
-        at=at
-    ).filter(life.setSize=songSetLimit)
+    allEvaluations = []
+    for evalution in MAP.objects.filter(at=at):
+        if evalution.life.setSize == songSetLimit:
+            allEvaluations.append(evalution)
     evaluationValues = [
         float("{0:.4f}".format(evalution.value))
         for evalution in allEvaluations
@@ -179,7 +194,12 @@ def value_gBar(songSetLimit, at=5):
     if not os.path.exists(directory):
         os.makedirs(directory)
     plt.figure()
-    plt.title('MAP - Mean Averange Precision@' + str(at) + '\nSet - '+ str(songSetLimit))
+    plt.title(
+        'MAP - Mean Averange Precision@'
+        + str(at)
+        + '\nSet - '
+        + str(songSetLimit)
+    )
     plt.ylabel('Intervalor de valores')
     plt.xlabel('Quantidade')
     plt.bar(
