bb4abc7c7777cb89d9a78fa726aa82dd7aa5dff3
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-23 00:51:48 -0300

    deletando codigo comentado

--- apps/evaluators/views.py
+++ apps/evaluators/views.py
@@ -8,11 +8,8 @@ from .NDCG.analyzer.views import runAllNDCGAnalizers
 
 def runEvaluations(songSetLimit, at=5):
     runMAP(at=at)
-    # runAllMAPAnalizers(at=at, songSetLimit=songSetLimit)
     runMRR(at=at)
-    # runAllMRRAnalizers(at=at, songSetLimit=songSetLimit)
     runNDCG(at=at)
-    # runAllNDCGAnalizers(at=at, songSetLimit=songSetLimit)
 
 
 def runAnalizerEvaluations(songSetLimit, at=5):
