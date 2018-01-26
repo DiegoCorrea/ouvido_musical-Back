699dae9714c343b1b6d9f6a0b7ef0ae16a1c955e
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-25 23:23:12 -0300

    benchmark de todos os tamanhos

--- apps/evaluators/MAP/analyzer/benchmark.py
+++ apps/evaluators/MAP/analyzer/benchmark.py
@@ -35,7 +35,7 @@ def bench_gLine(songSetLimit, at=5):
         + str(len(benchmarkTimes))
     )
     directory = str(
-        './files/apps/evaluators/MAP/graphs/'
+        '/files/apps/evaluators/MAP/graphs/'
         + str(songSetLimit)
         + '/benchmark/'
         + str(at) + '/'
@@ -106,7 +106,7 @@ def bench_gScatter(songSetLimit, at=5):
         + str(len(benchmarkTimes))
     )
     directory = str(
-        './files/apps/evaluators/MAP/graphs/'
+        '/files/apps/evaluators/MAP/graphs/'
         + str(songSetLimit)
         + '/benchmark/'
         + str(at) + '/'
@@ -165,7 +165,7 @@ def bench_gBoxPlot(songSetLimit, at=5):
         + str(len(benchmarkTimes))
     )
     directory = str(
-        './files/apps/evaluators/MAP/graphs/'
+        '/files/apps/evaluators/MAP/graphs/'
         + str(songSetLimit)
         + '/benchmark/'
         + str(at) + '/'
@@ -204,7 +204,7 @@ def bench_gBar(songSetLimit, at=5):
     mode = benchmarkCountList.most_common(1)[0][0]
     logger.debug('MAP Benchmark -> Mode: ' + str(mode))
     directory = str(
-        './files/apps/evaluators/MAP/graphs/'
+        'files/apps/evaluators/MAP/graphs/'
         + str(songSetLimit)
         + '/benchmark/'
         + str(at) + '/'
@@ -231,3 +231,60 @@ def bench_gBar(songSetLimit, at=5):
     )
     plt.close()
     logger.info("[Finish Bench MAP (Graph Bar)]")
+
+# ###################################################################### #
+
+
+def all_bench_gLine(at=5):
+    logger.info("[Start Bench MAP (Graph Line)]")
+    allBenchmarks = {}
+    for evalution in MAP.objects.filter(at=at):
+        if evalution.life.setSize not in allBenchmarks:
+            allBenchmarks.setdefault(evalution.life.setSize, [])
+        else:
+            allBenchmarks[evalution.life.setSize].append(
+                (
+                    evalution.benchmap.finished_at - evalution.benchmap.started_at
+                ).total_seconds() / 60.0
+            )
+    directory = str(
+        'files/apps/evaluators/MAP/graphs/all/'
+        + 'benchmark/'
+        + str(at) + '/'
+    )
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    plt.figure()
+    plt.grid(True)
+    plt.title(
+        'MAP - Mean Averange Precision@'
+        + str(at)
+        + '\nBenchmark'
+    )
+    plt.xlabel('ID da execução')
+    plt.ylabel('Tempo de execução (minutos)')
+    plt.plot(
+        [i for i in range(len(allBenchmarks[1000]))],
+        [benchmark for benchmark in allBenchmarks[1000]],
+        color='red',
+        label='1000'
+    )
+    plt.plot(
+        [i for i in range(len(allBenchmarks[2000]))],
+        [benchmark for benchmark in allBenchmarks[2000]],
+        color='green',
+        label='2000'
+    )
+    plt.plot(
+        [i for i in range(len(allBenchmarks[3000]))],
+        [benchmark for benchmark in allBenchmarks[3000]],
+        color='blue',
+        label='3000'
+    )
+    plt.legend(loc='best')
+    plt.savefig(
+        str(directory)
+        + 'all_value_gLine.png'
+    )
+    plt.close()
+    logger.info("[Finish Bench MAP (Graph Line)]")
