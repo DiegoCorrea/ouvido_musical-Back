e448b8c0600ed98dddaad7387a156046021ba4f7
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-28 15:10:17 -0300

    benchmark em segundos

--- apps/similarities/Cosine/analyzer/benchmark.py
+++ apps/similarities/Cosine/analyzer/benchmark.py
@@ -3,31 +3,96 @@ import logging
 import os
 
 from random import sample
-from django.utils import timezone
-from apps.data.songs.models import Song
 
-from apps.similarities.Cosine.algorithm.algorithm import CosineSimilarity
 from apps.similarities.Cosine.benchmark.models import BenchCosine_SongTitle
-from apps.similarities.Cosine.algorithm.models import CosineSimilarity_SongTitle
 
 logger = logging.getLogger(__name__)
-
-
-def runCosine(allSongs):
-    startedAt = timezone.now()
-    CosineSimilarity([song.title for song in allSongs])
-    return timezone.now() - startedAt
 
 
 def all_bench_gLine(size_list=[1500, 3000, 4500]):
     logger.info("[Start Bench Cosine (Graph Line)]")
     allBenchmarks = {}
-    songs_list = Song.objects.all()
     for runner in size_list:
-        if runner not in allBenchmarks:
-            allBenchmarks.setdefault(runner, [])
-        allBenchmarks[runner].append(
-            (
-                runCosine(allSongs=sample(set(songs_list), runner))
-            ).total_seconds() / 60.0
-        )
+        allBenchmarks.setdefault(runner, [])
+        for benchmark in BenchCosine_SongTitle.objects.filter(setSize=runner):
+            allBenchmarks[runner].append(
+                (
+                    benchmark.finished_at - benchmark.started_at
+                ).total_seconds()
+            )
+    directory = str(
+        'files/apps/similarities/Cosine/graphs/all/'
+    )
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    plt.figure()
+    plt.grid(True)
+    plt.title(
+        'Title Cosine Similarity'
+        + '\nBenchmark'
+    )
+    plt.xlabel('ID da execução')
+    plt.ylabel('Tempo de execução (Segundos)')
+    plt.plot(
+        [i+1 for i in range(len(allBenchmarks[size_list[0]]))],
+        [benchmark for benchmark in allBenchmarks[size_list[0]]],
+        color='red',
+        label=size_list[0]
+    )
+    plt.plot(
+        [i+1 for i in range(len(allBenchmarks[size_list[1]]))],
+        [benchmark for benchmark in allBenchmarks[size_list[1]]],
+        color='green',
+        label=size_list[1]
+    )
+    plt.plot(
+        [i+1 for i in range(len(allBenchmarks[size_list[2]]))],
+        [benchmark for benchmark in allBenchmarks[size_list[2]]],
+        color='blue',
+        label=size_list[2]
+    )
+    plt.legend(loc='best')
+    plt.savefig(
+        str(directory)
+        + 'all_bench_gLine.png'
+    )
+    plt.close()
+    logger.info("[Finish Bench Csine (Graph Line)]")
+
+
+def all_bench_gBoxPlot(at=5, size_list=[1500, 3000, 4500]):
+    logger.info("[Start Bench Cosine (Graph BoxPlot)]")
+    allBenchmarks = {}
+    for runner in size_list:
+        allBenchmarks.setdefault(runner, [])
+        for benchmark in BenchCosine_SongTitle.objects.filter(setSize=runner):
+            allBenchmarks[runner].append(
+                (
+                    benchmark.finished_at - benchmark.started_at
+                ).total_seconds()
+            )
+    directory = str(
+        'files/apps/similarities/Cosine/graphs/all/'
+    )
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    plt.figure()
+    plt.title(
+        'Title Cosine Similarity'
+        + '\nBenchmark'
+    )
+    plt.ylabel('Tempo de execução (Segundos)')
+    plt.boxplot(
+        [
+            [benchmark for benchmark in allBenchmarks[size_list[0]]],
+            [benchmark for benchmark in allBenchmarks[size_list[1]]],
+            [benchmark for benchmark in allBenchmarks[size_list[2]]]
+        ],
+        labels=[size_list[0], size_list[1], size_list[2]]
+    )
+    plt.savefig(
+        str(directory)
+        + 'all_bench_gBoxPlot.png'
+    )
+    plt.close()
+    logger.info("[Finish Bench Cosine (Graph BoxPlot)]")
