824ee47515d6cba7e9f46b859b7098df19070795
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-28 14:51:48 -0300

    variavel no model

--- apps/similarities/Cosine/benchmark/models.py
+++ apps/similarities/Cosine/benchmark/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 
 class BenchCosine_SongTitle(models.Model):
+    setSize = models.IntegerField(default=0)
     started_at = models.DateTimeField()
     finished_at = models.DateTimeField()
