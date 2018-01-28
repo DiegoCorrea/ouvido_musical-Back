ae5c070cf98a54fd516b4c6a18b48521aa4e1266
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-28 16:50:52 -0300

    fazendo a chamada na view

--- apps/similarities/Cosine/analyzer/views.py
+++ apps/similarities/Cosine/analyzer/views.py
@@ -7,14 +7,19 @@ from .benchmark import (
     all_bench_gLine,
     all_bench_gBoxPlot
 )
-from apps.CONSTANTS import COSINE_TOTAL_RUN, COSINE_SET_SIZE
+from apps.CONSTANTS import SET_SIZE
 
 
-def runAlgorithmAnalizers(size_list=COSINE_SET_SIZE):
+def runAlgorithmAnalizers(size_list):
     all_similarity_gLine(size_list=size_list)
     all_similarity_gBoxPlot(size_list=size_list)
 
 
-def runBenchmarkAnalizers(size_list=COSINE_SET_SIZE):
+def runBenchmarkAnalizers(size_list):
     all_bench_gLine(size_list=size_list)
     all_bench_gBoxPlot(size_list=size_list)
+
+
+def runCosineAnalizers(size_list=SET_SIZE):
+    runAlgorithmAnalizers(size_list=size_list)
+    runBenchmarkAnalizers(size_list=size_list)
