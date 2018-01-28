0f49360373517a3ea1e668b1474ae8bda4cb8b2c
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-28 15:10:23 -0300

    criando chamada

--- apps/similarities/Cosine/analyzer/views.py
+++ apps/similarities/Cosine/analyzer/views.py
@@ -3,7 +3,17 @@ from .algorithm import (
     similarity_gLine
 )
 
+from .benchmark import (
+    all_bench_gLine,
+    all_bench_gBoxPlot
+)
+
 
 def runAlgorithmAnalizers():
     similarity_gScatter()
     similarity_gLine()
+
+
+def runBenchmarkAnalizers(size_list=[1500, 3000, 4500]):
+    all_bench_gLine(size_list=size_list)
+    all_bench_gBoxPlot(size_list=size_list)
