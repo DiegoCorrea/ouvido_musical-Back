7331924c94797912f6cf3c8e8a069d9707d631c7
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-28 20:37:14 -0300

    lintando

--- apps/data/songs/models.py
+++ apps/data/songs/models.py
@@ -14,7 +14,9 @@ class Song(models.Model):
 
     def getSimilaries(self, songIDList):
         right_b = self.SongSimilarity_right.filter(songBase_id__in=songIDList)
-        right_c = self.SongSimilarity_right.filter(songCompare_id__in=songIDList)
+        right_c = self.SongSimilarity_right.filter(
+            songCompare_id__in=songIDList
+        )
         left_b = self.SongSimilarity_left.filter(songBase_id__in=songIDList)
         left_c = self.SongSimilarity_left.filter(songCompare_id__in=songIDList)
         return (((left_c | left_b) | right_c) | right_b)
@@ -24,6 +26,7 @@ class Song(models.Model):
             song_id=self.id,
             title=self.title,
         )
+
 
 class SongSimilarity(models.Model):
     # IDS
