eda6416a94786fb65b46d955a7505a4a93f23822
Diego Correa <diegocorrea.cc@gmail.com>
2018-01-28 20:28:39 -0300

    aumentando tamanho do arquivo de log

--- ouvidoMusical/settings.py
+++ ouvidoMusical/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'django_extensions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
