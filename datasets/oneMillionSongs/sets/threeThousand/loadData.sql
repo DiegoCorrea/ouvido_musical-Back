b67604830e87e913b4065c2d0923ac0a800d90fe
Diego Corrêa <diegocorrea.cc@gmail.com>
2018-08-03 22:16:18 -0300

    voltando versões

--- datasets/oneMillionSongs/sets/threeThousand/loadData.sql
+++
@@ -1,3 +0,0 @@
-\COPY songs_song FROM 'config/data/oneMillionSongs/threeThousand/songs.csv' DELIMITER ',' CSV HEADER;
-\COPY users_user FROM 'config/data/oneMillionSongs/threeThousand/users.csv' CSV HEADER;
-\COPY "userPlaySong_userplaysong"(user_id,song_id,play_count) FROM 'config/data/oneMillionSongs/threeThousand/playCount.csv' DELIMITER ',' CSV HEADER;
