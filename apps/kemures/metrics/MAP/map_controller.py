0036e515dcb92adf9baf8e5ab47c93996089bdbd
Diego Corrêa <diegocorrea.cc@gmail.com>
2018-08-13 21:06:09 -0300

    map refatorado

--- apps/kemures/metrics/MAP/map_controller.py
+++ apps/kemures/metrics/MAP/map_controller.py
@@ -1,21 +1,19 @@
+# Python and Pip Modules Calls
 import logging
-
 import numpy as np
 from django.utils import timezone
-
+# Application Calls
 from apps.kemures.recommenders.UserAverage.DAO.models import UserAverageLife
 from apps.kemures.metrics.MAP.DAO.models import MAP
 from apps.kemures.metrics.MAP.runtime.models import MAPRunTime
-
 from apps.kemures.kernel_var import AT_LIST
-
-logger = logging.getLogger(__name__)
 
 
 class MAPController:
     def __init__(self, evaluated_recommendations_df, at_size_list=AT_LIST):
-        self.evaluated_recommendations_df = evaluated_recommendations_df
-        self.at_size_list = at_size_list
+        self.__logger = logging.getself.__logger(__name__)
+        self.__evaluated_recommendations_df = evaluated_recommendations_df
+        self.__at_size_list = at_size_list
 
     def __get_ap_from_list(self, relevance_array):
         n_relevances = len(relevance_array)
@@ -33,35 +31,34 @@ class MAPController:
         else:
             return 0.0
 
-    def __calc_users_map(self, at):
-        logger.info("[Start User MAP]")
-        ap = []
-        for user in self.evaluated_recommendations_df['user_id'].unique().tolist():
-            __user_recommendation_model = self.evaluated_recommendations_df.loc[self.evaluated_recommendations_df['user_id'] == user]
+    def __calc_users_metric(self, at):
+        self.__logger.info("[Calculating Users MAP]")
+        users_metric_result_list = []
+        for user in self.__evaluated_recommendations_df['user_id'].unique().tolist():
+            __user_recommendation_model = self.__evaluated_recommendations_df.loc[self.__evaluated_recommendations_df['user_id'] == user]
             __user_recommendation_model.sort_values(by=['similarity'], ascending=False)
-            ap.append(self.__get_ap_from_list(__user_recommendation_model['iLike'].tolist()[:at]))
-        map_result = np.mean(ap)
-        logger.debug("Mean Average Precision@%d: %f", at, map_result)
-        logger.debug("Total Users Rated: %d", len(ap))
-        logger.info("[Finish User MAP]")
-        return map_result
+            users_metric_result_list.append(self.__get_ap_from_list(__user_recommendation_model['iLike'].tolist()[:at]))
+        metric_result = np.mean(users_metric_result_list)
+        self.__logger.debug("Mean Average Precision@%d: %f", at, metric_result)
+        self.__logger.debug("Total Users Rated: %d", len(users_metric_result_list))
+        return metric_result
 
-    def run_map(self, at):
-        logger.info("[Start Run MAP]")
+    def run_metric(self, at):
+        self.__logger.info("[Start Run MAP]")
         started_at = timezone.now()
-        value = self.__calc_users_map(at=at)
+        value = self.__calc_users_metric(at=at)
         finished_at = timezone.now()
-        map_result = MAP.objects.create(
+        metric_result_obj = MAP.objects.create(
             life=UserAverageLife.objects.last(),
             value=value,
             at=at
         )
         MAPRunTime.objects.create(
-            id=map_result,
+            id=metric_result_obj,
             started_at=started_at,
             finished_at=finished_at
         )
-        logger.info(
+        self.__logger.info(
             "Run Time[ "
             + str(value)
             + " ]: Start at - "
@@ -69,8 +66,8 @@ class MAPController:
             + " || Finished at -"
             + str(finished_at)
         )
-        logger.info("[Finish Run MAP]")
+        self.__logger.info("[Finish Run MAP]")
 
-    def run_full_map(self):
-        for at in self.at_size_list:
-            self.run_map(at=at)
+    def run_for_all_at_size(self):
+        for at in self.__at_size_list:
+            self.run_metric(at=at)
