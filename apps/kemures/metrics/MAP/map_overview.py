816d21ba17098c1dc29cb0230491beb05332fc99
Diego Corrêa <diegocorrea.cc@gmail.com>
2018-09-10 03:07:38 -0300

    map gerando novos graficos

--- apps/kemures/metrics/MAP/map_overview.py
+++ apps/kemures/metrics/MAP/map_overview.py
@@ -5,7 +5,7 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from apps.kemures.kernel.config.global_var import AT_LIST, SONG_SET_SIZE_LIST, MAP_PATH_GRAPHICS
+from apps.kemures.kernel.config.global_var import METADATA_OPTION_GRAPH, AT_LIST, SONG_SET_SIZE_LIST, MAP_PATH_GRAPHICS, METADATA_TO_PROCESS_LIST
 from apps.kemures.kernel.round.models import Round
 from apps.kemures.metrics.MAP.DAO.models import MAP
 from apps.kemures.metrics.MAP.runtime.models import MAPRunTime
@@ -13,28 +13,21 @@ from apps.kemures.metrics.MAP.runtime.models import MAPRunTime
 
 class MAPOverview:
     def __init__(self, song_set_size_list=SONG_SET_SIZE_LIST, at_size_list=AT_LIST,
-                 directory_to_save_graphics=MAP_PATH_GRAPHICS):
+                 directory_to_save_graphics=MAP_PATH_GRAPHICS, metadata_to_process=METADATA_TO_PROCESS_LIST):
         self.__logger = logging.getLogger(__name__)
         self.__directory_to_save_graphics = str(directory_to_save_graphics)
         if not os.path.exists(self.__directory_to_save_graphics):
             os.makedirs(self.__directory_to_save_graphics)
         self.__at_size_list = at_size_list
+        self.__metadata_to_process = metadata_to_process
         self.__song_set_size_list = song_set_size_list
         rounds_df = pd.DataFrame.from_records(list(Round.objects.all().values()))
+        rounds_df = rounds_df.drop(columns=['finished_at', 'started_at'])
         metric_df = pd.DataFrame.from_records(list(MAP.objects.all().values()))
         metric_run_time_df = pd.DataFrame.from_records(list(MAPRunTime.objects.all().values()))
-        self.__metric_results_collection_df = pd.DataFrame()
-        self.__metric_results_collection_df['song_set_size'] = metric_df['round_id']
-        self.__metric_results_collection_df['value'] = metric_df['value']
-        self.__metric_results_collection_df['at'] = metric_df['at']
-        self.__metric_results_collection_df['started_at'] = metric_run_time_df['started_at']
-        self.__metric_results_collection_df['finished_at'] = metric_run_time_df['finished_at']
-        for size in self.__song_set_size_list:
-            life_size_df = rounds_df.loc[rounds_df['song_set_size'] == size]
-            life_id_list = life_size_df['id'].tolist()
-            self.__metric_results_collection_df['song_set_size'] = [size if x in life_id_list else x for x in
-                                                                    self.__metric_results_collection_df[
-                                                                        'song_set_size']]
+        self.__metric_results_collection_df = metric_df.copy()
+        self.__metric_results_collection_df = self.__metric_results_collection_df.join(metric_run_time_df.set_index('id_id'), on='id')
+        self.__metric_results_collection_df = self.__metric_results_collection_df.join(rounds_df.set_index('id'), on='round_id')
 
     def make_time_graphics(self):
         self.__all_time_graph_line()
@@ -151,3 +144,60 @@ class MAPOverview:
             )
             plt.close()
         self.__logger.info("[Finish MAP Overview - Results - (Graph Box Plot)]")
+
+    def make_graphics_by_metadata(self):
+        self.__by_metadata_results_graph_line()
+        self.__by_metadata_results_graph_box_plot()
+        self.__save_csv()
+
+    def __by_metadata_results_graph_line(self):
+        self.__logger.info("[Start MAP Overview - Results - (Graph Line)]")
+        plt.figure()
+        plt.grid(True)
+        plt.xlabel('Metadado')
+        plt.ylabel('Valor')
+        for metadata, style in zip(self.__metadata_to_process, METADATA_OPTION_GRAPH):
+            at_df = self.__metric_results_collection_df[
+                self.__metric_results_collection_df['metadata_used'] == metadata]
+            at_df.sort_values("at")
+            plt.plot(
+                at_df['at'],
+                at_df['value'],
+                style,
+                label=metadata
+            )
+        plt.legend(loc='best')
+        plt.xticks(self.__at_size_list)
+        plt.savefig(
+            self.__directory_to_save_graphics
+            + 'map_by_metadata_results_graph_line_'
+            + '.png'
+        )
+        plt.close()
+        self.__logger.info("[Finish MAP Overview - Results - (Graph Line)]")
+
+    def __by_metadata_results_graph_box_plot(self):
+        self.__logger.info("[Start MAP Overview - Results - (Graph Box Plot)]")
+        plt.figure()
+        plt.grid(True)
+        plt.xlabel('Metadado')
+        plt.ylabel('valor')
+        box_plot_matrix = []
+        for metadata in self.__metadata_to_process:
+            at_df = self.__metric_results_collection_df[
+                self.__metric_results_collection_df['metadata_used'] == metadata]
+            box_plot_matrix.append([value for value in at_df['value'].tolist()])
+        plt.boxplot(
+            box_plot_matrix,
+            labels=self.__metadata_to_process
+        )
+        plt.savefig(
+            self.__directory_to_save_graphics
+            + 'map_by_metadata_results_graph_box_plot_'
+            + '.png'
+        )
+        plt.close()
+        self.__logger.info("[Finish MAP Overview - Results - (Graph Box Plot)]")
+
+    def __save_csv(self):
+        self.__metric_results_collection_df.to_csv(self.__directory_to_save_graphics + 'MAP.csv')
