b9c10b5e047719de0eda25c25d27eb2d52a2ec1c
Diego Corrêa <diegocorrea.cc@gmail.com>
2018-10-13 01:40:21 -0300

    chamada

--- apps/tecnics/content_based_metadata/main_experiments.py
+++ apps/tecnics/content_based_metadata/main_experiments.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import gc
 
-from apps.tecnics.content_based_metadata.main_by_model_length import with_pre_load_data_set_and_song_variation
+from apps.tecnics.content_based_metadata.main_by_model_length import \
+    pre_load_data_set_and_song_variation_all_combination
 from apps.tecnics.content_based_metadata.main_by_song_metadata import with_pre_load_data_set_and_user_variation
 
-with_pre_load_data_set_and_song_variation()
-gc.collect()
-with_pre_load_data_set_and_user_variation()
+
+def call():
+    with_pre_load_data_set_and_user_variation()
+    gc.collect()
+    pre_load_data_set_and_song_variation_all_combination()
